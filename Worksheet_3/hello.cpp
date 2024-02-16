50cf2b61f4cef9e3f1d57175cc2e696cd9bfdb0b
Ryder Taylor <eeyrt5@nottingham.ac.uk>
2024-02-16 16:05:28 +0000

    worksheet 3 inclusion?

---
+++ Worksheet_3/hello.cpp
@@ -0,0 +1,7 @@
+// behin - hello.cpp --------------
+#include <iostream>
+int main(int argc, char* argv[]) {
+	std::cout << "Hello Planet Earth!" << std::endl;
+	return 0;
+}
+// end - hello.cpp -----------------
