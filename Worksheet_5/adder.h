fa533a0738694e29ffda0bb01625ac257fd07749
Ryder Taylor <eeyrt5@nottingham.ac.uk>
2024-02-19 00:36:09 +0000

    Doxygen comment update test

---
+++ Worksheet_5/adder.h
@@ -0,0 +1,17 @@
+// adder.h
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+* This file contains the declerations of all exported functions in the maths library.
+*/
+
+/** Adding function					[Brief description]
+* This function adds two numbers		[More detail]
+* @param a is the first number		[Parameter definition]
+* @param b is the second number		[Parameter defintiion]
+* @return sum of a and b			[Return value description]
+*/
+int add(int a, int b);
+
+#endif
