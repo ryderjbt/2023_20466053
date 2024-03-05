c2fd04790a68a4f261f5039dcb1f2be5766d4a6b
Ryder Taylor <eeyrt5@nottingham.ac.uk>
2024-03-05 16:24:38 +0000

    Buttons working

--- Worksheet_6/Worksheet6_Exercise1_Qt/mainwindow.cpp
+++ Worksheet_6/Worksheet6_Exercise1_Qt/mainwindow.cpp
@@ -1,14 +1,29 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect( ui->pushButton, &QPushButton::released, this, &MainWindow::handleButton1);
+    connect( ui->pushButton_2, &QPushButton::released, this, &MainWindow::handleButton2);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::handleButton1() {
+    QMessageBox msgBox;
+    msgBox.setText("Button1 was clicked");
+    msgBox.exec();
+}
+
+void MainWindow::handleButton2() {
+    QMessageBox msgBox;
+    msgBox.setText("Button2 was clicked");
+    msgBox.exec();
 }
--- Worksheet_6/Worksheet6_Exercise1_Qt/mainwindow.h
+++ Worksheet_6/Worksheet6_Exercise1_Qt/mainwindow.h
@@ -12,6 +12,9 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+public slots:
+    void handleButton1();
+    void handleButton2();
 
 public:
     MainWindow(QWidget *parent = nullptr);
